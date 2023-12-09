506564485ab779bd23a457523200cd455c7b70e3
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-12-09 17:00:03 -0500

    MondoDB
    Se añade configuracion para conectar mongo con nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+
+    //referencia a la base de datos
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'), //el argumengo es la url de la base de datos
     PokemonModule,
   ],
   controllers: [],
