28d01f9a1e559a611a093573457bc51863dcac31
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-12-09 17:15:15 -0500

    MondoDB
    Se crea entidad de pokemon y se defina el modelo dentro del modulo de pokemon para usarlo en table plus

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,22 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  // id: string //mongo lo da
+
+  @Prop({
+    unique: true,
+    index: true, //sabe donde está el elemento que estaos buscando
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+//exportamos el equema para que se tengan las definiciones de lo que queremos en la base de datos
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
