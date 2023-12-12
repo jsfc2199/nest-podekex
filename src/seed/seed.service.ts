956ae7b409fc3016a9d6ee4ffd365ccc7c569989
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-12-11 19:46:49 -0500

    MongoDB
    Se ejecuta sobre el seed la inserción de pokemones de manera mas optimizada para ejecutar un solo insert para toda la data

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -14,14 +14,29 @@ export class SeedService {
     private readonly pokemonModel: Model<Pokemon>,
   ) {}
   async executeSeed() {
+    await this.pokemonModel.deleteMany({}); //equivale al delete * from pokemons y queremos limpiar la data cada que ejecutemos el seed
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
-    data.results.forEach(async ({ name, url }) => {
+    // Forma 1 para crear simultanemante inserciones
+    // const insertPromisesArray = [];
+    // data.results.forEach(({ name, url }) => {
+    //   const segmets = url.split('/');
+    //   const no: number = +segmets[segmets.length - 2]; //el id del pokemon es el penultimo segmento
+    //   // await this.pokemonModel.create({ name, no }); //creamos por lotes los pokemon
+    //   insertPromisesArray.push(this.pokemonModel.create({ name, no }));
+    // });
+
+    // await Promise.all(insertPromisesArray);
+
+    //forma dos
+    const pokemonToInsert: { name: string; no: number }[] = [];
+    data.results.forEach(({ name, url }) => {
       const segmets = url.split('/');
-      const no: number = +segmets[segmets.length - 2]; //el id del pokemon es el penultimo segmento
-      await this.pokemonModel.create({ name, no }); //creamos por lotes los pokemon
+      const no: number = +segmets[segmets.length - 2];
+      pokemonToInsert.push({ name, no });
     });
+    await this.pokemonModel.insertMany(pokemonToInsert);
     return 'seed executed';
   }
 }
