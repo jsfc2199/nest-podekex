0bd8f03a557992932db6ab97f7ffa84d67bc5f01
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-12-14 12:06:29 -0500

    MongoDB
    Se inicia configuracion para varaibles de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,20 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
     //referencia a la base de datos
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'), //el argumengo es la url de la base de datos
-    PokemonModule, CommonModule, SeedModule,
+    MongooseModule.forRoot(process.env.MONGODB), //el argumengo es la url de la base de datos
+    PokemonModule,
+    CommonModule,
+    SeedModule,
   ],
   controllers: [],
   providers: [],
